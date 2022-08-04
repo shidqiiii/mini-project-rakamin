6074d747718e9221ca301ebcece1fee6e3af43b6
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-08-04 23:46:17 +0700

    Penjelasan jenis - jenis react component
    Terdiri dari 2 component yaitu Functional (stateless) dan Class (statefull)

---
+++ src/Components/HelloWorld.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+// Class Component
+class HelloWorld extends Component {
+    render() {
+        return (
+            <h1>Hello {this.props.name}</h1>
+        )
+    }
+}
+
+// Functional Component
+// function HelloWorld(props) {
+//     return (
+//         <h1>Hello {props.name}</h1>
+//     )
+// }
+
+
+
+export default HelloWorld
