eec25b2053176a508225ecb2dfd1cd257e9e6c91
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-08-05 08:55:22 +0700

    menambahkan tailwind kedalam file project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
 import CounterExample from './Components/CounterExample';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 import HelloWorld from './Components/HelloWorld'
 
 function App() {
   return (
     <div>
-      {/* <HelloWorld name="Albert" /> */}
+      <Header />
 
-      <CounterExample />
+      <HelloWorld name="Albert" />
+
+      <Footer />
+
+      {/* <CounterExample /> */}
     </div>
   );
 }
--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -4,7 +4,9 @@ import React, { Component } from 'react'
 class HelloWorld extends Component {
     render() {
         return (
-            <h1>Hello {this.props.name}</h1>
+            <>
+                <h1>Hello {this.props.name}</h1>
+            </>
         )
     }
 }
