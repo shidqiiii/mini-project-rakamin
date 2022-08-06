378ad31702408dacdfa8873ab8f2e20e210511ca
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-08-07 00:55:01 +0700

    Mengambil data dari API dan menggunakan route params
    Menggunakan API dan fitur react-hook useParams

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HelloWorld from './Components/HelloWorld'
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import Home from './Views/Home';
 import About from './Views/About';
+import Product from './Views/Product';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/products/:id' element={<Product />} />
           </Routes>
         </div>
 
